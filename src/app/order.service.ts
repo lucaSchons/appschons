8e404764e671c2dafba0bf1d117ac9b49d25f5a2
lucaSchons <lucasgschons@gmail.com>
2024-02-02 15:16:54 -0300

    Change

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -17,7 +17,7 @@ export class OrderService implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   getOrder() {
     return this.order;
@@ -25,7 +25,6 @@ export class OrderService implements OnInit {
 
   addProduct(produto: ProdutoEncomenda) {
     const novoPedido: pedidoItem[] = [...this.orderSubject.value];
-    console.log("NOVO PEDIDO NO INICIO ", novoPedido);
     const produtosLocalStorage = localStorage.getItem('@schons');
 
     if (localStorage.getItem('contador')) {
@@ -36,29 +35,27 @@ export class OrderService implements OnInit {
     } else {
       this.contadorSubject.next(this.contadorSubject.value + 1)
     }
-    
+
     if (produtosLocalStorage !== null) {
       const objeto = JSON.parse(produtosLocalStorage);
       const instancia = objeto;
-      console.log("PRIMEIRO IF ", instancia)
       if (objeto.length > 0) {
         this.orderSubject.next(objeto);
       }
-     
+
       if (instancia !== undefined) {
         const itemExistente = objeto.find((item: pedidoItem) => item.items.produto.some(p => p.descricao_produto === produto.descricao));
-        
-        if (itemExistente !== undefined ) {
+
+        if (itemExistente !== undefined) {
           const quantidade = itemExistente.items.produto[0].quantity;
           const varNewOrder: pedidoItem[] = [...this.orderSubject.value];
-          
-          if(varNewOrder.length === 0){
+
+          if (varNewOrder.length === 0) {
             itemExistente.items.produto[0].quantity = quantidade + 1;
             varNewOrder.push(itemExistente);
-          }else{
+          } else {
             const index = varNewOrder.findIndex(item => item.items.produto[0].descricao_produto === produto.descricao);
             if (index !== -1) {
-              console.log("QUANTIDADE DE VAR NEW ORDER ", quantidade);
               varNewOrder[index].items.produto[0].quantity = quantidade + 1;
             }
           }
@@ -66,7 +63,7 @@ export class OrderService implements OnInit {
           this.orderSubject.next(varNewOrder);
           localStorage.setItem('@schons', JSON.stringify(varNewOrder));
 
-        } else{
+        } else {
           const novoItem: pedidoItem = {
             items: {
               produto: [{
@@ -88,7 +85,7 @@ export class OrderService implements OnInit {
           localStorage.setItem('@schons', JSON.stringify(novoPedido));
         }
       }
-      
+
     } else {
 
       const novoItem: pedidoItem = {
@@ -114,64 +111,56 @@ export class OrderService implements OnInit {
 
     this.order.subscribe(result => { });
     localStorage.setItem('contador', JSON.stringify(this.contadorSubject.value));
-    
+
   }
 
-  // decrementar(descricao: string) {
-  //   const novoQuantidadeProduto = { ...this.quantidadeProdutoSubject.value };
-  //   const quantidadeStorage = localStorage.getItem('@schons');
+  removeProduct(produto: ProdutoEncomenda) {
+    const novoPedido: pedidoItem[] = [...this.orderSubject.value];
+    console.log("REMOVE INICIO", novoPedido);
+    const produtosLocalStorage = localStorage.getItem('@schons');
 
-  //   if (localStorage.getItem('contador')) {
-  //     const quantidadeString = localStorage.getItem('contador');
-  //     const quantidade = quantidadeString ? +quantidadeString : 0;
-  //     let quantidade_ext = quantidade;
-  //     console.log("quantidade vinda do storage", quantidade_ext);
-  //     console.log("produto quantidade", novoQuantidadeProduto[descricao]);
-  //     if(quantidade_ext >= 2 && novoQuantidadeProduto[descricao] !== 0){
-  //       const resultado = quantidade_ext -1;
-  //       console.log("resultado", resultado);
-  //       this.contadorSubject.next(resultado);
-  //     }else if(quantidade_ext === 1 && novoQuantidadeProduto[descricao] !== 0){
-  //       this.contadorSubject.next(0);
-  //     }
-  //   }
+    if (localStorage.getItem('contador')) {
+      const quantidadeString = localStorage.getItem('contador');
+      const quantidade = quantidadeString ? +quantidadeString : 0;
+      let quantidade_ext = quantidade;
+      if (quantidade_ext >= 2) {
+        const resultado = quantidade_ext - 1;
+        this.contadorSubject.next(resultado);
+      } else if (quantidade_ext === 1) {
+        this.contadorSubject.next(0);
+      }
+    }
 
-  //   if (quantidadeStorage !== null) {
-  //     const quantidadeObj = JSON.parse(quantidadeStorage);
-  //     const quantidade = quantidadeObj[descricao];
+    localStorage.setItem('contador', JSON.stringify(this.contadorSubject.value));
 
-  //     if(quantidade >= 2 && novoQuantidadeProduto[descricao] !== 0){
-  //       const resultado = quantidade -1;
-  //       novoQuantidadeProduto[descricao] = resultado;
-  //     }else if(quantidade === 1 && novoQuantidadeProduto[descricao] !== 0){
-  //       alert("tem certeza que deseja excluir item?");
-  //       novoQuantidadeProduto[descricao] = 0;
-  //     }
-  //     for (const chave in quantidadeObj) {
-  //       if (quantidadeObj.hasOwnProperty(chave) && !novoQuantidadeProduto.hasOwnProperty(chave)) {
-  //         novoQuantidadeProduto[chave] = quantidadeObj[chave];
-  //       }
-  //     }
+    if (produtosLocalStorage !== null) {
+      const objeto = JSON.parse(produtosLocalStorage);
+      const instancia = objeto;
+      if (objeto.length > 0) {
+        this.orderSubject.next(objeto);
+      }
 
-  //   } else {
+      if (instancia !== undefined) {
+        const itemExistente = objeto.find((item: pedidoItem) => item.items.produto.some(p => p.descricao_produto === produto.descricao));
 
-  //     if (!novoQuantidadeProduto[descricao] || novoQuantidadeProduto[descricao] === 0) {
-  //       alert("seu produto está zerado");
-  //       novoQuantidadeProduto[descricao] = 0;
-  //     } 
-  //     // else if (novoQuantidadeProduto[descricao] === 1) {
-  //     //   alert("tem certeza que deseja excluir item?");
-  //     //   novoQuantidadeProduto[descricao] = 0;
-  //     // } else {
-  //     //   novoQuantidadeProduto[descricao]--;
-  //     // }
-  //   }  
+        if (itemExistente !== undefined) {
+          const quantidade = itemExistente.items.produto[0].quantity;
+          const varNewOrder: pedidoItem[] = [...this.orderSubject.value];
+          const index = varNewOrder.findIndex(item => item.items.produto[0].descricao_produto === produto.descricao);
 
-  //   this.quantidadeProdutoSubject.next(novoQuantidadeProduto);
-  //   localStorage.setItem('contador', JSON.stringify(this.contadorSubject.value))
-  //   this.quantidadeProduto.subscribe(result =>{
-  //     this.produto = result;
-  //   });
-  //   localStorage.setItem('@schons', JSON.stringify(this.produto));
-  // }
+          if (quantidade >= 2 && index !== -1) {
+            varNewOrder[index].items.produto[0].quantity = quantidade - 1;
+            this.orderSubject.next(varNewOrder);
+          } else if(quantidade === 1 && index !== -1) {
+            alert("Tem certeza que deseja excluir item?");
+            varNewOrder[index].items.produto[0].quantity = 0;
+            this.orderSubject.next(varNewOrder);
+          }
+          
+          localStorage.setItem('@schons', JSON.stringify(varNewOrder));
+        }
+      }
+    }
+  }
+
 }
